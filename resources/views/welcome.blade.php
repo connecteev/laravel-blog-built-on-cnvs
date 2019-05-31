5f8cf9e39d3a97bc4d02ab3e3bce4a1b94914baf
Kunal Punjabi <kunalspunjabi@gmail.com>
2019-05-30 23:35:16 -0700

    update links on home/welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,17 +81,13 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel Canvas Blog
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="/canvas/posts">Canvas Admin (Posts)</a>
+                    <a href="/canvas">Canvas Stats</a>
+                    <a href="/blog">Front-end Blog</a>
                 </div>
             </div>
         </div>
